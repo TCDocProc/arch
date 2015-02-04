8059b4190cfa6e64772de546ee33dc3ad96bc382
Kevin Bluett <bluettk@tcd.ie>
2015-02-04 23:47:16 +0000

    Added initial views

--- app/website/urls.py
+++ app/website/urls.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'website.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+ 	url(r'^$', 'members.views.index', name='index'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
