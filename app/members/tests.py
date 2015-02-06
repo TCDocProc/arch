de434d022b6ba4b91f5ce1f12b37dc20604f2cdc
Kevin Bluett <bluettk@tcd.ie>
2015-02-06 12:31:28 +0000

    Added first test case

--- app/members/tests.py
+++ app/members/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+class TestTestCase(TestCase):
+    def setUp(self):
+        self.truthy = True
+
+    def test_true(self):
+        """Ensures that the value of true holds"""
+        self.assertEqual(self.truthy, True)
