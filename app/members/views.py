d1fca3717c42424e0232cc3e737524db4454ba91
Conor Murphy <conor.smurf@gmail.com>
2015-02-18 17:38:40 +0000

    Cleaned up and removed uneccessary templates

--- app/members/views.py
+++ app/members/views.py
@@ -3,11 +3,9 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 def index(request):
-	#
     return HttpResponse(loader.get_template('index.html').render(Context({})))
 
 @login_required(login_url='/')
 def authenticated_page(request):
-	#
-	context = RequestContext(request, {})
-	return HttpResponse(loader.get_template('auth.html').render(context))
+    context = RequestContext(request, {})
+    return HttpResponse(loader.get_template('auth.html').render(context))
