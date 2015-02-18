b19be662f8081a79d82e1223b0fbd943ff95a620
Conor Brady <conorjbrady1@gmail.com>
2015-02-18 22:21:46 +0000

    Removed templates and added failsafe xml

--- app/processes/views.py
+++ app/processes/views.py
@@ -2,11 +2,16 @@ from django.http import HttpResponse
 
 import xml.etree.cElementTree as et
 import requests, json, re
+from website import settings
 
 def index(request):
 
     r = requests.get("http://proisis.lero.ie/~jnoll/carepathways/peos.cgi")
-    xml = et.fromstring(r.text)
+    xml = ""
+    if r.status_code == 200:
+        xml = et.fromstring(r.text)
+    else:
+        xml = et.fromstring(open(settings.STATIC_ROOT+"/xml/pathways.xml", "r").read())
 
     response = [ _parse_process(process) for process in xml.findall("./process_table/process") ]
 
@@ -15,10 +20,11 @@ def index(request):
 def _parse_process(process):
     return { "id"       : process.attrib["pid"],
              "type"     : re.search(r'\./(.*)\.pml',process.attrib["model"]).group(1),
-             "sequences": _parse_seq(process) }
+             "sequence" : _parse_seq(process) }
 
 def _parse_action(elem):
     return { "type"        : "action",
+             "name"        : elem.attrib["name"],
              "description" : re.sub(r'(\t|(<br>)|(\n\(null\)\n)|(\")|(\A\n))', '',elem.find("script").text),
              "state"       : elem.attrib["state"]}
 
@@ -31,7 +37,7 @@ def _parse_seq(seq):
     p = []
 
     for elem in seq.findall("*"):
-        
+
         if elem.tag == "action":
             p.append(_parse_action(elem))
         elif elem.tag == "branch":
