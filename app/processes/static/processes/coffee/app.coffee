8af581088e679aafb34edb7af680dcb3d9801633
Conor Brady <conorjbrady1@gmail.com>
2015-04-08 15:31:49 +0100

    Added delete button to process view

--- app/processes/static/processes/coffee/app.coffee
+++ app/processes/static/processes/coffee/app.coffee
@@ -392,3 +392,30 @@ jQuery ->
 
         ss.insertRule ".focused > .sequence { max-width: #{ $(window).width() - 80}px; max-height: #{ $(window).height() - 100}px; }", ss.cssRules.length
         ss.insertRule ".sequence.fill { max-width: #{ $(window).width() - 20}px }", ss.cssRules.length
+
+
+    # Delete button stuff
+
+    getCookie = (name) ->
+        cookieValue = null
+        if document.cookie and document.cookie != ''
+            cookies = document.cookie.split(';')
+            i = 0
+            while i < cookies.length
+                cookie = jQuery.trim(cookies[i])
+                # Does this cookie string begin with the name we want?
+                if cookie.substring(0, name.length + 1) == name + '='
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
+                    break
+                i++
+        cookieValue
+
+    $.ajaxSetup
+        beforeSend: (xhr) ->
+            xhr.setRequestHeader "X-CSRFToken", getCookie('csrftoken')
+
+    $("#delete_pathway").on 'click', ->
+        $.ajax
+            type: 'DELETE',
+            url: '/processes',
+            success: -> location.reload()
--- app/processes/views.py
+++ app/processes/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader, Context
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import ensure_csrf_cookie
 from core.models import *
 import os.path
 
@@ -11,28 +12,40 @@ import requests, json, re
 from website import settings
 
 @login_required(login_url='/openemr/signup/')
+@ensure_csrf_cookie
 def index(request,extension):
 
     pathways = Pathway.objects.filter(user_id=request.user.id).order_by('-id')
 
-    if pathways:
-        if os.path.isfile(settings.MEDIA_ROOT+'/'+str(pathways[0].pathway_xml)):
-            xml = et.fromstring(open(settings.MEDIA_ROOT+'/'+str(pathways[0].pathway_xml), "r").read())
+    if request.method=="GET":
 
-            response = [ _parse_process(process) for process in xml.findall("./process_table/process") ]
+        if pathways:
 
-            if(extension=="json"):
-                return HttpResponse(json.dumps(response), content_type='application/json')
+            if os.path.isfile(settings.MEDIA_ROOT+'/'+str(pathways[0].pathway_xml)):
+                xml = et.fromstring(open(settings.MEDIA_ROOT+'/'+str(pathways[0].pathway_xml), "r").read())
 
+                response = [ _parse_process(process) for process in xml.findall("./process_table/process") ]
+
+                if(extension=="json"):
+                    return HttpResponse(json.dumps(response), content_type='application/json')
+
+                else:
+                    context = RequestContext(request, { "data": response })
+                    return HttpResponse(loader.get_template('process.html').render(context))
             else:
-                context = RequestContext(request, { "data": response })
-                return HttpResponse(loader.get_template('process.html').render(context))
+                pathways[0].delete()
+                return HttpResponseRedirect('/add_pathway/' )
         else:
-            pathways[0].delete()
-            return HttpResponseRedirect('/add_pathway/' )
-    else:
-        return HttpResponseRedirect( '/add_pathway/' )
+            return HttpResponseRedirect( '/add_pathway/' )
 
+    elif request.method=="DELETE":
+
+        pathways.delete()
+        return HttpResponse("", status=204)
+
+    else:
+
+        return HttpResponse("Method not allowed", status=405)
 
 def _parse_process(process):
     return { "id"       : process.attrib["pid"],
